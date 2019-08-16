fbd2a14f23b07fbffd62d4929f7c426ee2cbb2d2
ss <ss1057532022@gmail.com>
2019-08-17 01:15:19 +0800

    feat: code clean up

--- Search/a_star/a_star.cpp
+++ Search/a_star/a_star.cpp
@@ -2,7 +2,7 @@
     
     MIT License
 
-    Copyright (c) 2018-2019 SS47816@github.com.
+    Copyright (c) 2019-2020 SS47816
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
